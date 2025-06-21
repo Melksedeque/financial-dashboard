c53d1440ab182bc8ad023989bf45c7f815a214a0
Melksedeque <freelancer@melksedeque.com.br>
2025-06-21 15:20:30 -0300

    Adição de imagem Hero Mobile usando o componente Image do Next

--- app/page.tsx
+++ app/page.tsx
@@ -37,6 +37,13 @@ export default function Page() {
             className="hidden md:block"
             alt="Screenshot of the Dashboard project showing desktop version"
           />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshot of the Dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
