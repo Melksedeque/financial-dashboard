fcae3b4040c2fd970f8379a6e28331df43e07a1c
Melksedeque <freelancer@melksedeque.com.br>
2025-07-06 13:57:11 -0300

    Aplicar Suspense no card de LatestInvoices

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,11 +3,10 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons'
-import { fetchCardData, fetchLatestInvoices } from '@/app/lib/data';
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons'
+import { fetchCardData } from '@/app/lib/data';
  
 export default async function Page() {
-    const latestInvoices = await fetchLatestInvoices();
     const { numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices } = await fetchCardData();
 
   return (
@@ -25,7 +24,9 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>  
       </div>
     </main>
   );
